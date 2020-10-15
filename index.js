af5a85db3f355c40d8ddbcc9b5fac3280d90a28a
dylan-rinella <rinelladylan@aim.com>
2020-10-15 19:18:06 -0400

    almost done

--- index.js
+++ index.js
@@ -41,9 +41,23 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.stomach = [];
+    this.name = name;
+    this.age = age;
+  }
+  eat(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    } 
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `Name ${this.name}, Age: ${this.age}`;
+  }
 }
-
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
@@ -59,7 +73,16 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+    console.log(`You filled up with ${gallons} gallons! Now you have ${this.tank} gallons total.`)
+  }
 }
 
 /*
